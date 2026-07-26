7ced631a1372958ab8e9581a454b2b4ad91d8bad
NoxAevi <example@example.com>
2026-07-26 17:27:02 -0400

    e

--- modules/users/NoxAevi/NoxAevi.nix
+++ modules/users/NoxAevi/NoxAevi.nix
@@ -3,9 +3,11 @@
         isNormalUser = true;
         extraGroups = [ "wheel" ];
         home = "/home/NoxAevi";
-        
+        shell = pkgs.zsh;
         initialPassword = "user";
     };
+
+    programs.zsh.enable = true;
 
     home-manager.users.NoxAevi = import ./home.nix;
 }
